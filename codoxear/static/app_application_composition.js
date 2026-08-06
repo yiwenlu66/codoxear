79cd71ace9fee3b779f24a9b03ebd31eb7bdae75
Yiwen Lu <mcfloundinho@gmail.com>
2026-08-07 00:20:29 +0800

    Consolidate event bindings into application composition

--- codoxear/static/app_application_composition.js
+++ codoxear/static/app_application_composition.js
@@ -3,6 +3,22 @@
 (function installCodoxearApplicationComposition(global) {
   "use strict";
 
+  function createEventBindings(options = {}) {
+    const addEvent = options.addEvent;
+    if (typeof addEvent !== "function") throw new TypeError("event bindings dependency missing: addEvent");
+
+    function on(target, type, handler, listenerOptions) {
+      if (!target || typeof handler !== "function") throw new TypeError(`event binding requires ${type} target and handler`);
+      return addEvent(target, type, handler, listenerOptions);
+    }
+
+    function onClick(target, handler, listenerOptions) {
+      return on(target, "click", handler, listenerOptions);
+    }
+
+    return Object.freeze({ on, onClick });
+  }
+
   function createApplicationComposition(deps = {}) {
     const {
       window, document, navigator, HTMLElement, EventSource, AbortController, getComputedStyle,
@@ -1403,5 +1419,6 @@
   }
 
 
+  global.CodoxearEventBindings = Object.freeze({ createEventBindings });
   global.CodoxearApplicationComposition = Object.freeze({ createApplicationComposition });
 })(window);
